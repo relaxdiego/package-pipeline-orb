ef566a23b949e1a963f881aeedc614544f5404b0
Mark S. Maglana <mmaglana@gmail.com>
2021-09-05 21:49:01 +0800

    Check if CIRCLE_TOKEN is defined before publishing

--- src/scripts/providers/circleci-orb/publish.sh
+++ src/scripts/providers/circleci-orb/publish.sh
@@ -1,4 +1,11 @@
 Publish() {
+  if [ -z "$CIRCLE_TOKEN" ]; then
+      echo "FATAL: CIRCLE_TOKEN is not defined! To fix this error, make sure the" \
+           "job is given a Circle CI context that defines the CIRCLE_TOKEN env var." \
+           "See https://circleci.com/docs/2.0/contexts/ for more information."
+      exit 1
+  fi
+
   PIPELINE_BUILD_ID=$(jq -r .build_id BUILD-INFO)
   circleci orb publish \
     --skip-update-check \
