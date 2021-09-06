de9f21065c0b495a108f4fd13cda863a8ae946aa
Mark S. Maglana <mmaglana@gmail.com>
2021-09-06 21:15:09 +0800

    Actually trigger the vetting workflow

--- .circleci/pre-vetting/scripts/trigger-vetting-workflow.sh
+++ .circleci/pre-vetting/scripts/trigger-vetting-workflow.sh
@@ -2,17 +2,18 @@ set -euo pipefail
 
 BUILD_ID=$(jq -r .build_id ~/workspace/BUILD-INFO)
 echo $BUILD_ID
-# cat > pipelineparams.json \<<EOF
-# {
-#     "branch": "$CIRCLE_BRANCH",
-#     "parameters": {
-#         "run-post-dev-workflow": true,
-#         "version-to-test": "dev:${BUILD_ID}.dev"
-#     }
-# }
-# EOF
-# curl -u "$CIRCLE_TOKEN": \
-#      -X POST \
-#      --header "Content-Type: application/json" \
-#      -d @pipelineparams.json \
-#      "https://circleci.com/api/v2/project/gh/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline"
+
+cat > pipelineparams.json <<EOF
+{
+    "branch": "$CIRCLE_BRANCH",
+    "parameters": {
+        "vet-the-deb-build": true,
+        "orb-dev-build-id": "dev:${BUILD_ID}"
+    }
+}
+EOF
+curl -u "$CIRCLE_TOKEN": \
+     -X POST \
+     --header "Content-Type: application/json" \
+     -d @pipelineparams.json \
+     "https://circleci.com/api/v2/project/gh/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline"
