6dcdb297cd72c777ecc2cc173f7f991a93721986
Mark S. Maglana <mmaglana@gmail.com>
2021-09-07 07:38:00 +0800

    Append dev label when vetting a dev build

--- .circleci/pre-vetting/scripts/render-dev-build-vetting-config.sh
+++ .circleci/pre-vetting/scripts/render-dev-build-vetting-config.sh
@@ -1,6 +1,6 @@
 set -eo pipefail
 
-PIPELINE_BUILD_ID=$(jq -r .build_id ~/workspace/BUILD-INFO)
+PIPELINE_BUILD_ID=dev:$(jq -r .build_id ~/workspace/BUILD-INFO)
 
 # Poor man's rendering engine
 eval "echo \"$( cat .circleci/pre-vetting/templates/vetting-config.template.yml)\"" > vetting-config.yml
