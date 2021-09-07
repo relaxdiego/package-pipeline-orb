bcf009957e3d5854b11757ad77b68257893e4d77
Mark S. Maglana <mmaglana@gmail.com>
2021-09-07 21:56:58 +0800

    Re-arrange .circleci files and directories

--- .circleci/scripts/render-dev-build-vetting-config.sh
+++ .circleci/scripts/render-dev-build-vetting-config.sh
@@ -3,4 +3,4 @@ set -eo pipefail
 PIPELINE_BUILD_ID=dev:$(jq -r .build_id ~/workspace/BUILD-INFO)
 
 # Poor man's rendering engine
-eval "echo \"$( cat .circleci/pre-vetting/templates/vetting-config.template.yml)\"" > vetting-config.yml
+eval "echo \"$( cat .circleci/templates/vetting-config.template.yml)\"" > vetting-config.yml
