b2de904d88308537a3a262fdf3da49542300af12
Mark S. Maglana <mmaglana@gmail.com>
2021-09-12 20:40:22 +0800

    Replace the '+' in the docker tag with '--'

--- src/scripts/build/docker.sh
+++ src/scripts/build/docker.sh
@@ -5,9 +5,13 @@ Main() {
     PL_BUILD_INFO_PATH=${PL_BUILD_INFO_PATH:-"BUILD-INFO"}
     # The "eval echo" bit ensures that the path in the string is expanded.
     # NOTE that strings aren't normally expanded as per the POSIX standard
-    PL_TAG=$(jq -r .build_id "$(eval echo "$PL_BUILD_INFO_PATH")")
+    PL_TAG=$(jq -r .build_id "$(eval echo "$PL_BUILD_INFO_PATH")" | tr "+" "--")
+
+    set -x
 
     docker build -t "$PL_DOCKER_IMAGE_NAME:$PL_TAG" "$(eval dirname "$PL_DOCKERFILE_PATH")"
+
+    { set +x; } >/dev/null
 }
 
 
