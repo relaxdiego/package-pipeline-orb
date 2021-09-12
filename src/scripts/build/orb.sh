edbd2e80045daeaa07e89805e8799783e0fa58f2
Mark S. Maglana <mmaglana@gmail.com>
2021-09-12 20:47:04 +0800

    Move orb build script to src/scripts/build

---
+++ src/scripts/build/orb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+
+Main() {
+    set -x
+
+    # Prepend BUILD-INFO contents as a single-line comment
+    echo "# pipeline:build-info $(jq -c . ~/workspace/BUILD-INFO)" > orb.yml
+
+    circleci orb pack src >> orb.yml
+
+    { set +x; } >/dev/null
+
+    head -n5 orb.yml
+}
+
+
+# Will not run if sourced for bats-core tests. View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    Main
+fi
